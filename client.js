8d6b09571e7cf44a9c6bb943fea7939b97ec6bfa
Sergei Zavgorodnii <SZavgorodniy@alfabank.ru>
2019-10-25 17:29:56 +0300

    add rcon client and `print` function

--- client.js
+++ client.js
@@ -1,15 +1,78 @@
 const net = require('net');
+const Rcon = require('mbr-rcon');
 
 let port = 2038;
+let rconPort = 25575;
 
-module.exports = function (callback) {
+function Connect (callback) {
   const socket = net.Socket();
   socket.connect(port, function () {
     callback(socket);
   });
 }
-module.exports.port = function (newPort) {
+Connect.port = function (newPort) {
   port = newPort;
 
-  return module.exports;
+  return Connect;
 }
+
+function RconConnection (connection) {
+  this.connection = connection;
+}
+
+RconConnection.prototype.send = function (command) {
+  const connection = this.connection;
+
+  return new Promise(function (resolve, reject) {
+    connection.send(command, {
+      onSuccess: resolve,
+      onError: reject
+    });
+  });
+};
+
+RconConnection.prototype.stop = function () {
+  this.connection.close({
+    onError: function (error) {
+      throw error;
+    }
+  });
+}
+
+function RConnect (password, callback) {
+  const rcon = new Rcon({
+    port: rconPort
+  });
+
+  const connection = rcon.connect({
+    onError: function (error) {
+      throw error;
+    }
+  });
+
+  connection.auth({
+    password,
+    onSuccess: function () {
+      callback(new RconConnection(connection));
+    },
+    onError: function (error) {
+      throw error;
+    }
+  });
+}
+
+RConnect.port = function (newPort) {
+  rconPort = newPort;
+
+  return RConnect;
+}
+
+function print (data) {
+  console.log(data);
+}
+
+module.exports = {
+  Connect,
+  RConnect,
+  print
+};
--- test.js
+++ test.js
@@ -1,12 +1,17 @@
-const connect = require('./client.js');
+const { RConnect, print } = require('./client.js');
 
+/*
 connect(function (socket) {
-  /*
   socket.write('time set 6000\n');
   socket.write('gamerule doDaylightCycle false\n');
   socket.write('weather clear\n');
   socket.write('gamerule doWeatherCycle false\n');
   socket.write('gamemode spectator madbrozzer\n');
-  */
   socket.end();
+});
+*/
+
+RConnect(111, function (mc) {
+  mc.send('list').then(print);
+  mc.stop();
 });
