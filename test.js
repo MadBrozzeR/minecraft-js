0abfb1fe9e1914523be21d19bd9aad9f6dcfddce
MadBrozzeR <git.public.s@madbr.ru>
2019-10-26 19:48:34 +0300

    fix build script

--- test.js
+++ test.js
@@ -1,21 +1,27 @@
 const { block } = require('./lib/block.js');
-/*
 const { RConnect, print } = require('./client.js');
 const { player } = require('./lib/player.js');
 const { gamerule } = require('./lib/utils.js');
 
 RConnect('111', function (mc) {
+/*
   mc.send('list').then(print);
   gamerule(mc, {
     doWeatherCycle: false,
     doDaylightCycle: false
   }).then(print);
   mc.send('time set noon').then(print);
-
-  mc.stop();
-});
 */
+  const madbrozzer = player('madbrozzer', mc);
 
+  madbrozzer.build('~10 ~ ~10', {
+    s: block('stone'),
+    g: block('glass')
+  }, 'build.txt').then(function () {
+    mc.stop();
+  });
+});
+/*
 const { Palette } = require('./lib/build.js');
 
 const palette = new Palette({
@@ -27,3 +33,4 @@ palette.build([0, 0, 0], './build.txt', {
     console.log(result);
   }
 });
+*/
