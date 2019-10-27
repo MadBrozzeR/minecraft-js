913e0771bc04e6118aab4f1f10c8a3a920c1b184
MadBrozzeR <git.public.s@madbr.ru>
2019-10-27 11:30:03 +0300

    add poll

--- test.js
+++ test.js
@@ -2,16 +2,33 @@ const { block } = require('./lib/block.js');
 const { RConnect, print } = require('./client.js');
 const { player } = require('./lib/player.js');
 const { gamerule } = require('./lib/utils.js');
+const { poll } = require('./lib/poll.js');
+
+const walls = poll([
+  {
+    item: block('stone_bricks'),
+    weight: 8
+  },
+  {
+    item: block('stone'),
+    weight: 1
+  },
+  {
+    item: block('cracked_stone_bricks'),
+    weight: 1
+  }
+]);
 
 const palette = {
   ' ': block('air'),
-  w: block('stone_bricks'),
+  w: walls,
   f: block('acacia_planks'),
   o: block('glass'),
   d: block('acacia_door', {half: 'lower', facing: 'west'}),
   D: block('acacia_door', {half: 'upper', facing: 'west'}),
   s: block('stone'),
-  g: block('glass')
+  g: block('glass'),
+  t: block('stone_brick_stairs', {facing: 'west'})
 };
 
 RConnect('111', function (mc) {
